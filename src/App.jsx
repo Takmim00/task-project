205d9d35449f2d3b107ea6b642bd96ec469010d1
Takmim00 <takmimm@gmail.com>
2025-07-27 22:40:34 +0600

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  async function fetchData() {
+    try {
+      const res = await fetch(
+        "https://car-rental-server-kohl.vercel.app/cars/limit"
+      )
+      const data = res.json();
+      return(data)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(()=>{
+    console.log(fetchData());
+  })
+
+  const [car,setCar]=useState([]);
+  useEffect(()=>{
+    const fetchCar=async()=>{
+      try{
+        const res= await axios.get('https://car-rental-server-kohl.vercel.app/cars/limit')
+        setCar(res.data)
+        console.log(res);
+      }catch(err){
+        console.log(err);
+      }
+    }
+    fetchCar()
+  },[])
+  console.log(car);
   return (
     <>
       <div className="card bg-base-100 w-96 shadow-sm">
+        
         <figure>
           <img
             src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
